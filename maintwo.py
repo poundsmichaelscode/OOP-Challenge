f3a743b5760a29f689e5cee7573288d4514d392d
Olayenikan Michael M.O <138946028+poundsmichaelscode@users.noreply.github.com>
2025-04-17 13:03:01 +0100

    Create maintwo.py

---
+++ maintwo.py
@@ -0,0 +1,19 @@
+
+
+#Note: this is an additional file for the project display without emoji, and a direct display of the pet status 
+
+
+from pet import Pet
+
+# Create your pet
+my_pet = Pet("Buddy")
+
+# Interact with your pet
+my_pet.get_status()
+my_pet.eat()
+my_pet.play()
+my_pet.sleep()
+my_pet.train("roll over")
+my_pet.train("sit")
+my_pet.show_tricks()
+my_pet.get_status()
